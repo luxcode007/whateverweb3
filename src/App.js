399855bfeab8ae17555a8e03fe164bccabcee987
Edward Saviano <lyonz4prez@gmail.com>
2023-09-21 17:17:58 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <MainContent />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>My Web App</h1>
+      <ScrollingStrip items={['Headline 1', 'Headline 2', 'Headline 3']} />
+      <ScrollingStrip items={['Asset 1: $Price1', 'Asset 2: $Price2', 'Asset 3: $Price3']} />
+    </div>
+  );
+}
+
+function ScrollingStrip({ items }) {
+  return (
+    <div className="scrolling-strip">
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function MainContent() {
+  return (
+    <div className="container">
+      <Sidebar items={['Asset A: $PriceA', 'Asset B: $PriceB']} />
+      <Feed />
+      <Sidebar items={['Asset X: $PriceX', 'Asset Y: $PriceY']} />
+    </div>
+  );
+}
+
+function Sidebar({ items }) {
+  return (
+    <div className="sidebar">
+      <h3>Assets & Prices</h3>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Feed() {
+  // Replace with actual fetched data in the future
+  const newsItems = ['News item 1', 'News item 2', 'News item 3'];
+
+  return (
+    <div className="feed">
+      <h2>Latest News</h2>
+      <ul>
+        {newsItems.map((news, index) => (
+          <li key={index}>{news}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
